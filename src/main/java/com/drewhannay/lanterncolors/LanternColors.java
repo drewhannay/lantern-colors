b05023c9aba6730c6c680dd3f665e5908b464aaf
Drew Hannay <dhannay@linkedin.com>
2020-07-10 17:19:47 -0700

    Delete unused client & mod setup classes
    They came from the MDK template but it doesn't seem like we need to do
    anything in those events.

--- src/main/java/com/drewhannay/lanterncolors/LanternColors.java
+++ src/main/java/com/drewhannay/lanterncolors/LanternColors.java
@@ -2,10 +2,7 @@ package com.drewhannay.lanterncolors;
 
 import com.drewhannay.lanterncolors.blocks.ColoredLanternBlocks;
 import com.drewhannay.lanterncolors.items.ColoredLanternItems;
-import com.drewhannay.lanterncolors.setup.ClientSetup;
-import com.drewhannay.lanterncolors.setup.ModSetup;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static com.drewhannay.lanterncolors.LanternColors.MODID;
 
@@ -18,8 +15,5 @@ public class LanternColors {
     public LanternColors() {
         ColoredLanternBlocks.init();
         ColoredLanternItems.init();
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(ModSetup::init);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::init);
     }
 }

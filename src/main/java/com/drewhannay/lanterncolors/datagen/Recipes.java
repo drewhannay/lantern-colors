90b25029a98a5da0259f898c29c74617db3aba8e
Drew Hannay <dhannay@linkedin.com>
2020-07-10 17:01:01 -0700

    Add advancements/recipes json files
    Turns out you're supposed to directly extend the Minecraft
    RecipeProvider rather than extending the ForgeRecipeProvider class. The
    Forge one overrides the saveRecipeAdvancement method to do nothing, so
    these json files were never generated and the custom recipes weren't
    showing up in the in-game recipe book when you got the correct
    materials.
    
    Now we extend the correct class in our generator and the files are
    properly created. This also adjusts the criterion so that you only need
    to have a lantern to unlock the recipes and not glass panes.

--- src/main/java/com/drewhannay/lanterncolors/datagen/Recipes.java
+++ src/main/java/com/drewhannay/lanterncolors/datagen/Recipes.java
@@ -2,16 +2,14 @@ package com.drewhannay.lanterncolors.datagen;
 
 import com.drewhannay.lanterncolors.LanternColors;
 import com.drewhannay.lanterncolors.blocks.ColoredLanternBlocks;
-import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapelessRecipeBuilder;
-import net.minecraftforge.common.Tags;
-import net.minecraftforge.common.data.ForgeRecipeProvider;
 
 import java.util.function.Consumer;
 
-public class Recipes extends ForgeRecipeProvider {
+public class Recipes extends RecipeProvider {
 
     public Recipes(DataGenerator generatorIn) {
         super(generatorIn);
@@ -23,8 +21,7 @@ public class Recipes extends ForgeRecipeProvider {
             ShapelessRecipeBuilder.shapelessRecipe(block)
                                   .addIngredient(ItemTags.glassPaneTagForDyeColor(block.getDyeColor()))
                                   .addIngredient(ItemTags.LANTERNS)
-                                  .addCriterion("has_lantern", hasItem(Blocks.LANTERN))
-                                  .addCriterion("has_glass_pane", hasItem(Tags.Items.GLASS_PANES))
+                                  .addCriterion("has_lantern", hasItem(ItemTags.LANTERNS))
                                   .build(consumer);
         });
     }
